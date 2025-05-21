79787ccb3105717729f8eba9d2970ed4452a93e0
raihannurhidayat <grandrx041@gmail.com>
2025-05-21 19:24:17 +0700

    feat: create utils for undo redo feature and utils navigation

---
+++ utils/navigation_utils.py
@@ -0,0 +1,11 @@
+import os
+
+def go_up(current_path):
+    parent = os.path.dirname(current_path)
+    if parent and parent != current_path:
+        return parent
+    return current_path
+
+
+def is_valid_directory(path):
+    return os.path.isdir(path)
